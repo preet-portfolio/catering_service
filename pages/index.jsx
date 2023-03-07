b547d8470a7d895b6b617098a1afc0cf26550745
Preet Panchal <preet2020@icloud.com>
2023-03-07 11:51:51 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,112 +1,83 @@
-/* eslint-disable @next/next/no-img-element */
-import { motion } from "framer-motion";
-import { MenuIcon, UserIcon } from "@heroicons/react/solid";
-import { HeartIcon } from "@heroicons/react/outline";
-import Image from "next/image";
-const links = [
-  { name: "Open roles", href: "#" },
-  { name: "Internship program", href: "#" },
-  { name: "Our values", href: "#" },
-  { name: "Meet our leadership", href: "#" },
-];
-const stats = [
-  { name: "Offices worldwide", value: "12" },
-  { name: "Full-time colleagues", value: "300+" },
-  { name: "Hours per week", value: "40" },
-  { name: "Paid time off", value: "Unlimited" },
+const benefits = [
+  {
+    name: "Quality Food",
+    description: "Delicious and freshly made food to impress your guests.",
+  },
+  {
+    name: "Customization",
+    description:
+      "We can customize the menu to suit your preferences, dietary needs, and budget.",
+  },
+  {
+    name: "Convenience",
+    description:
+      "We take care of the food, so you can focus on your guests and enjoy the party.",
+  },
+  {
+    name: "Professional Service",
+    description:
+      "Our team is trained to provide excellent service and ensure your satisfaction.",
+  },
+  {
+    name: "Variety",
+    description:
+      "We offer a wide range of options, including appetizers, mains, desserts, and drinks.",
+  },
+  {
+    name: "Local Ingredients",
+    description:
+      "We use locally sourced, fresh ingredients to support the community and ensure quality.",
+  },
 ];
 
-const Home = () => {
+export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      <img
-        src="https://images.pexels.com/photos/6589/food-salad-vegetables-party.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      />
-      <svg
-        viewBox="0 0 1097 845"
-        aria-hidden="true"
-        className="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]"
-      >
-        <path
-          fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)"
-          fillOpacity=".2"
-          d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
-        />
-        <defs>
-          <linearGradient
-            id="10724532-9d81-43d2-bb94-866e98dd6e42"
-            x1="1097.04"
-            x2="-141.165"
-            y1=".22"
-            y2="363.075"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#776FFF" />
-            <stop offset={1} stopColor="#FF4694" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        viewBox="0 0 1097 845"
-        aria-hidden="true"
-        className="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0"
-      >
-        <path
-          fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)"
-          fillOpacity=".2"
-          d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
-        />
-        <defs>
-          <linearGradient
-            id="8ddc7edb-8983-4cd7-bccb-79ad21097d70"
-            x1="1097.04"
-            x2="-141.165"
-            y1=".22"
-            y2="363.075"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#776FFF" />
-            <stop offset={1} stopColor="#FF4694" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Work with us
+    <div className="bg-white">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Why Choose Ottawa Bites for Your House Party?
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+          <p className="mt-4 text-gray-500">
+            At Ottawa Bites, we understand the importance of a good party.
+            That&apos;s why we offer quality food, customized menus,
+            professional service, and more to make your house party a success.
           </p>
-        </div>
-        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            {links.map((link) => (
-              <a key={link.name} href={link.href}>
-                {link.name} <span aria-hidden="true">&rarr;</span>
-              </a>
-            ))}
-          </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse">
-                <dt className="text-base leading-7 text-gray-300">
-                  {stat.name}
-                </dt>
-                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">
-                  {stat.value}
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {benefits.map((benefit) => (
+              <div key={benefit.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{benefit.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">
+                  {benefit.description}
                 </dd>
               </div>
             ))}
           </dl>
         </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+          <img
+            src="https://images.pexels.com/photos/3807044/pexels-photo-3807044.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+            className="rounded-lg bg-gray-100 saturate-190"
+          />
+          <img
+            src="https://images.pexels.com/photos/3807054/pexels-photo-3807054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Top down view of walnut card tray with embedded magnets and card groove."
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src="https://images.pexels.com/photos/3807029/pexels-photo-3807029.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Side of walnut card tray with card groove and recessed card area."
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src="https://images.pexels.com/photos/3807027/pexels-photo-3807027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Walnut card tray filled with cards and card angled in dedicated groove."
+            className="rounded-lg bg-gray-100"
+          />
+        </div>
       </div>
     </div>
   );
-};
-
-export default Home;
+}
