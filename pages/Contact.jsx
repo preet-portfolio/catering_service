696ac48d25988913f0a0c80eab57165914e168a3
Preet Panchal <preet2020@icloud.com>
2023-03-10 12:35:29 -0500

    main

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import { useState } from "react";
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+function ContactPage() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact;
+  function handleInputChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Here you can add code to submit the form data to a backend
+    setIsSubmitted(true);
+  }
+
+  return (
+    <div className="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      {isSubmitted ? (
+        <p className="text-green-500 mb-4">Thank you for your message!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              className="border-gray-400 border-2 rounded-md p-2 w-full"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="border-gray-400 border-2 rounded-md p-2 w-full"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              rows="5"
+              className="border-gray-400 border-2 rounded-md p-2 w-full"
+              required
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default ContactPage;
